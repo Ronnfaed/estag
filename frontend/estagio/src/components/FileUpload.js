e4079591778861a8b9b68d48cc103828f652b062
Ronnfaed <prinzantiochus@gmail.com>
2024-06-20 23:33:49 -0300

    basic CSS halfway done

--- frontend/estagio/src/components/FileUpload.js
+++ frontend/estagio/src/components/FileUpload.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './FileUpload.css';
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
   const [jsonResult, setJsonResult] = useState(null);
+  const [uploading, setUploading] = useState(false);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -11,6 +13,13 @@ const FileUpload = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!file) {
+      alert('Por favor, selecione um arquivo PDF.');
+      return;
+    }
+
+    setUploading(true);
 
     const formData = new FormData();
     formData.append('pdf', file);
@@ -25,20 +34,28 @@ const FileUpload = () => {
       setJsonResult(response.data);
     } catch (error) {
       console.error('Error uploading file: ', error);
+    } finally {
+      setUploading(false);
     }
   };
 
   return (
-    <div>
-      <h2>Upload PDF File</h2>
+    <div className="file-upload-container">
+      <h2 style = {{textAlign: "center"}}>Upload de Arquivo PDF</h2>
       <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Enviar</button>
+        <div className="file-input-container">
+          <input type="file" onChange={handleFileChange} />
+        </div>
+        <div>
+        <button type="submit" disabled={uploading} style = {{justifyItems:"center"}}>
+            {uploading ? 'Enviando...' : 'Enviar'}
+        </button>
+        </div>
       </form>
 
       {jsonResult && (
-        <div>
-          <h3>JSON Result</h3>
+        <div className="json-result-container">
+          <h3>Resultado em JSON</h3>
           <pre>{JSON.stringify(jsonResult, null, 2)}</pre>
         </div>
       )}
